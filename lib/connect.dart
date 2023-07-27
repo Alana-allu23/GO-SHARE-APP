962dd951822c60731723273b5a952aa2b958ab64
alana <alanakoranath@gmmail.com>
2023-07-27 17:16:43 +0530

    first commit

---
+++ lib/connect.dart
@@ -0,0 +1,11 @@
+// class con{
+//   static const url="http://192.168.68.149/coride-api/";
+//   static const url1="http://192.168.68.149/go_share_admin/";
+//   static const uploads="http://192.168.68.149/go_share_admin/uploads/";
+
+// }
+class con {
+  static const url = "http://172.20.10.6/New/GO-SHARE-Api/";
+  static const url1 = "http://172.20.10.6/New/GO-SHARE-Admin/";
+  static const uploads = "http://172.20.10.6/New/GO-SHARE-Admin/uploads/";
+}
